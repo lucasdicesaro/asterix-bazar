c5a6fa8e207031e1b611af025e728fa4d05c4f26
lucasdicesaro <lucasdicesaro@d3c0cedc-9d4d-0410-b2cd-271dfcd79d49>
2008-05-21 23:38:45 +0000

    Solucionado c++ compiler $(PACKAGE_CFLAGS)

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -1,21 +1,20 @@
-//#include <libxml/parser.h>			
-#include <stdio.h>
+
+#include <libxml++/libxml++.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+using namespace xmlpp;
 
 int main()
 {				
-/*	xmlDocPtr doc;                                                                                                                  
-	doc = xmlParseFile ("testfile.xml");                                                                                                          
-	if (doc == NULL) {
-		printf ("Document not parsed successfully. \n");
-		return -1;
-	}
-	else {
-		printf ("Document parsed successfully.\n");
-		xmlFreeDoc(doc);
-		return 0;
-	}*/;
+	xmlpp::Element* el;
+	char* attr_name;	
+	//assert(el);
+	Attribute* attr = el->get_attribute(attr_name);
+//	return (attr == NULL)? "" : attr->get_value();
 	
 //~/Projects/asterix-bazar/src$ g++ -DHAVE_CONFIG_H -I. -I.. -DPACKAGE_LOCALE_DIR=\""/usr/local//locale"\" -DPACKAGE_SRC_DIR=\""."\" -DPACKAGE_DATA_DIR=\""/usr/local/share"\" -I/usr/include/  -I/usr/include/libxml2   -g -O2 -MT asterix_bazar-main.o -MD -MP -MF .deps/asterix_bazar-main.Tpo -c -o asterix_bazar-main.o `test -f 'main.cpp' || echo './'`main.cpp
-
+//-I/usr/include/libxml2 -I/usr/include/libxml++-2.6 -I/usr/includes/libglibmm-2.4
 	
 }
