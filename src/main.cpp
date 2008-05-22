12b784fc2962841841d8a98fc999471ea9786736
lucasdicesaro <lucasdicesaro@d3c0cedc-9d4d-0410-b2cd-271dfcd79d49>
2008-05-22 06:46:43 +0000

    Archivo Configuracion

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,14 @@
 
-#include <libxml++/libxml++.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+#include "tools.h"
+#include <iostream>
 
-using namespace xmlpp;
+using namespace std;
 
 int main()
 {				
-	xmlpp::Element* el;
-	char* attr_name;	
-	//assert(el);
-	Attribute* attr = el->get_attribute(attr_name);
-//	return (attr == NULL)? "" : attr->get_value();
-	
-//~/Projects/asterix-bazar/src$ g++ -DHAVE_CONFIG_H -I. -I.. -DPACKAGE_LOCALE_DIR=\""/usr/local//locale"\" -DPACKAGE_SRC_DIR=\""."\" -DPACKAGE_DATA_DIR=\""/usr/local/share"\" -I/usr/include/  -I/usr/include/libxml2   -g -O2 -MT asterix_bazar-main.o -MD -MP -MF .deps/asterix_bazar-main.Tpo -c -o asterix_bazar-main.o `test -f 'main.cpp' || echo './'`main.cpp
-//-I/usr/include/libxml2 -I/usr/include/libxml++-2.6 -I/usr/includes/libglibmm-2.4
-	
+	Tools tools;
+	tools.Config_Parser("lista_participante.conf");
+	Tools::debug(tools.get_IP_De_Participante("LaPetisita"));
+	Tools::debug(tools.get_IP_De_Participante("ElTano"));
+	Tools::debug(tools.get_IP_De_Participante("ElNegro"));
 }
