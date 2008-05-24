f6b79c2febc3095f8c3b1099db309ff51d3c9fb6
lucasdicesaro <lucasdicesaro@d3c0cedc-9d4d-0410-b2cd-271dfcd79d49>
2008-05-24 17:25:30 +0000

    Se estructuro el manejo de hilos

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,94 @@
 
 #include "comprador.h"
 #include "vendedor.h"
+#include "common/tools.h"
 #include <iostream>
+#include <pthread.h>
+#include <signal.h>
 
-using namespace std;
+void sig_handler(int id);
+void* proc_comprador(void* param);
+void* proc_vendedor(void* param);
+//using namespace std;
 
 int main()
 {				
-	Comprador comprador;
-	comprador.initialize();
+	signal(SIGTERM, sig_handler);
+	signal(SIGINT, sig_handler);
 	
-	Vendedor vendedor;
-	vendedor.initialize();
+//	Tools tools;
+//	tools.Config_Parser("lista_participante.conf");
+//	Tools::debug("ElTano");
+//	Tools::debug(tools.get_IP_De_Participante("ElTano"));
+	
+	Comprador* comprador = Comprador::instance();
+	Vendedor* vendedor = Vendedor::instance();
+	
+	pthread_t thr_comprador, thr_vendedor;
+	pthread_create(&thr_comprador, NULL, proc_comprador, comprador);
+	pthread_create(&thr_vendedor, NULL, proc_vendedor, vendedor);
+	
+	// Wait threads:
+	pthread_join(thr_comprador, NULL);
+	pthread_join(thr_vendedor, NULL);
+	
+	Tools::debug("main. Finished");
 }
+
+void sig_handler(int id)
+{
+	if (id == SIGINT) 
+	{
+		Tools::debug("Se recibio la señal SIGINT");
+	}
+	
+	Tools::debug("Termination Signal arrived");
+	Event ev;
+	ev.id = QUIT;
+	Comprador::instance()->post_event(ev, true);
+	Vendedor::instance()->post_event(ev, true);
+}
+
+void* proc_comprador(void* param)
+{
+	Tools::debug("proc_comprador: enter.");
+
+	try
+	{	
+		Comprador* comprador = (Comprador*) param;
+		comprador->run();
+	} catch (...)
+	{
+		printf("Exception on comprador\n");
+		exit(2);
+	}
+
+	Tools::debug("proc_comprador: leave.");
+	return NULL;
+}
+
+
+void* proc_vendedor(void* param)
+{
+	Tools::debug("proc_vendedor: enter.");
+
+	try
+	{	
+		Vendedor* vendedor = (Vendedor*) param;
+		vendedor->run();
+	} catch (...)
+	{
+		printf("Exception on vendedor\n");
+		exit(2);
+	}
+
+	Tools::debug("proc_vendedor: leave.");
+	return NULL;
+}
+
+
+
+
+
+
+
--- src/vendedor.h
+++ src/vendedor.h
@@ -1,13 +1,29 @@
 #ifndef _VENDENDOR_H_
 #define _VENDENDOR_H_
 
-class Vendedor
+#include "common/runner.h"
+#include "common/types.h"
+
+class Vendedor : public Runner
 {
 	public:
-		void initialize();
+		/**
+		Singleton
+		*/
+		static Vendedor* instance();
+		/**
+		* Implementacion requerida por Runner
+		*/
+		virtual void on_event(const Event& ev);
 		
+		void procesar_look_up(const void *event_tag);
+		void procesar_buy(const void *event_tag);
+		void procesar_pre_quit(const void *event_tag);
+	
 	protected:
-		
+		Vendedor();
+		~Vendedor();
+		static Vendedor* single_instance;				
 };
 
 #endif //_VENDENDOR_H_
