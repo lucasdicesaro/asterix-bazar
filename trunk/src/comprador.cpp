0e90a9be534241cbccfb6fc9df198ff946bb6c75
lucasdicesaro <lucasdicesaro@d3c0cedc-9d4d-0410-b2cd-271dfcd79d49>
2008-05-24 17:25:30 +0000

    Se estructuro el manejo de hilos

--- trunk/src/comprador.cpp
+++ trunk/src/comprador.cpp
@@ -1,16 +1,102 @@
 
-#include "comprador.h"
+#include "comprador.h" 
+#include "vendedor.h"  // Quitar esto despues de las pruebas
 #include "common/tools.h"
 #include <iostream>
 
+
+Comprador* Comprador::single_instance = NULL;
+
 /**
-initialize
+* Constructors
 */
-void Comprador::initialize()
+Comprador::Comprador()
 {
-	Tools tools;
-	tools.Config_Parser("lista_participante.conf");
-	Tools::debug("ElTano");
-	Tools::debug(tools.get_IP_De_Participante("ElTano"));
 }
 
+Comprador::~Comprador()
+{
+}
+
+Comprador* Comprador::instance()
+{
+	if (!single_instance)
+		single_instance = new Comprador;
+	return single_instance;
+}
+
+void Comprador::on_event(const Event& ev)
+{
+	switch (ev.id)
+	{
+		case INIT:
+			Tools::debug("Comprador: on_event: INIT:");
+			procesar_init(ev.tag);
+			break;
+			
+			
+		// El cliente no recibe nunca LOOKUP
+		//case LOOKUP:
+		//	Tools::debug("Comprador: on_event: LOOKUP:");
+		//	// Inicia la rutina de pedido de compra
+		//	break;
+			
+			
+		case REPLAY:
+			Tools::debug("Comprador: on_event: REPLAY:");
+			procesar_replay(ev.tag);
+			break;
+			
+		// El cliente no recibe nunca BUY
+		//case BUY:
+		//	Tools::debug("Comprador: on_event: BUY:");
+			// Inicia la rutina de compra			
+		//	break;	
+			
+		case PRE_QUIT:
+			Tools::debug("Comprador: on_event: QUIT:");
+			procesar_pre_quit(ev.tag);
+			break;
+			
+		default:
+			Tools::debug("Comprador: on event: *UNKNOWN*.");
+			break;
+	}
+}
+
+
+void Comprador::procesar_init(const void *event_tag) 
+{
+	// Solo para prueba, despues quitar, porque el Comprador no va a estar en el mismo proceso que en Vendedor
+	const char* producto = "Sal";
+	Event ev;
+	ev.id  = LOOKUP;
+	ev.tag = producto;	
+	Vendedor::instance()->post_event(ev, true);
+}
+
+
+
+
+void Comprador::procesar_replay(const void *event_tag) 
+{
+	// Procesa la respuesta del vendedor							
+	// connect() al vendedor
+	const char *vendedor = (char *)event_tag;
+	Tools::debug (vendedor);
+	
+	Event ev;
+	ev.id  = BUY;
+	ev.tag = vendedor;	
+	Vendedor::instance()->post_event(ev, true);
+}
+
+
+void Comprador::procesar_pre_quit(const void *event_tag) 
+{
+	// Inicia la rutina de salida	
+	Event ev;
+	ev.id  = QUIT;
+	this->post_event(ev, true);
+	//raise(SIGTERM);
+}
--- trunk/src/comprador.h
+++ trunk/src/comprador.h
@@ -1,13 +1,29 @@
 #ifndef _COMPRADOR_H_
 #define _COMPRADOR_H_
 
-class Comprador
+#include "common/runner.h"
+#include "common/types.h"
+
+class Comprador : public Runner
 {
 	public:
-		void initialize();
+		/**
+		Singleton
+		*/
+		static Comprador* instance();
+		/**
+		* Implementacion requerida por Runner
+		*/
+		virtual void on_event(const Event& ev);
+		
+		void procesar_init(const void *event_tag);
+		void procesar_replay(const void *event_tag);
+		void procesar_pre_quit(const void *event_tag);
 		
 	protected:
-		
+		Comprador();
+		~Comprador();
+		static Comprador* single_instance;				
 };
 
 #endif //_COMPRADOR_H_
