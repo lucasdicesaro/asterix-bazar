8e8507f03487461b09eac461400df3b4c9b1c9ca
lucasdicesaro <lucasdicesaro@d3c0cedc-9d4d-0410-b2cd-271dfcd79d49>
2008-05-24 15:49:22 +0000

    Modificaciones debido a la incorporacion de common y las clases Comprador y Vendedor

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -1,14 +1,15 @@
 
-#include "tools.h"
+#include "comprador.h"
+#include "vendedor.h"
 #include <iostream>
 
 using namespace std;
 
 int main()
 {				
-	Tools tools;
-	tools.Config_Parser("lista_participante.conf");
-	Tools::debug(tools.get_IP_De_Participante("LaPetisita"));
-	Tools::debug(tools.get_IP_De_Participante("ElTano"));
-	Tools::debug(tools.get_IP_De_Participante("ElNegro"));
+	Comprador comprador;
+	comprador.initialize();
+	
+	Vendedor vendedor;
+	vendedor.initialize();
 }
