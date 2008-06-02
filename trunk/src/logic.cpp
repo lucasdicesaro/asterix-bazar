9e6a24ca05f26319f558ee1aa97ae6b2cb7c8fca
lucasdicesaro <lucasdicesaro@d3c0cedc-9d4d-0410-b2cd-271dfcd79d49>
2008-06-02 06:19:12 +0000

    - Se agregaron atributos para levantar del archivo de configuracion - Desarrollo para el intercambio de paquetes

--- trunk/src/logic.cpp
+++ trunk/src/logic.cpp
@@ -6,13 +6,16 @@
 #include "router.h"
 #include <assert.h>
 
+extern char *nombre_nodo;
+
 Logic* Logic::single_instance = NULL;
-bool primeraVez = true;
 /**
 * Constructors
 */
 Logic::Logic()
 {
+	ReconnectParamsDS *reconnectParams = Tools::instance()->get_reconnect_params();
+	hopcount = reconnectParams->hopcount;
 }
 
 Logic::~Logic()
@@ -28,29 +31,54 @@ Logic* Logic::instance()
 
 void Logic::on_event(const Event& ev)
 {
+	Event evBroad;
+	Mensaje *mensaje = NULL;
+	std::string vendedor;
 	switch (ev.id)
 	{
 		case INIT:
 			Tools::debug("Logic: on_event: INIT:");
-			// TODO
-			// Preparar el inicio del Logic			
-			// Definir cuando inicia el look up, teniendo en cuenta que ya no se define un rol.
-			// Puede ser vendedor y comprador a la vez.
+			break;
+			
+		case DO_LOOKUP:
+			Tools::debug("Logic: on_event: DO_LOOKUP:");
+			// TODO definir los criterios del producto a elegir para comprar y la cantidad
+			mensaje = build_look_up_msg(PRODUCTO_SAL, 2);
+			add_nodo(mensaje, nombre_nodo);
+			
+			evBroad.id = BROADCAST;
+			evBroad.tag = Tools::duplicate(mensaje->to_string());
+			Router::instance()->post_event(evBroad, true);			
+			break;
+			
+		case DO_REPLY:
+			Tools::debug("Logic: on_event: DO_REPLY:");
+			// TODO definir 
+			mensaje = build_reply_msg();
+			//add_nodo(mensaje, nombre_nodo);			
+			
+			evBroad.id = BROADCAST;
+			evBroad.tag = Tools::duplicate(mensaje->to_string());
+			Router::instance()->post_event(evBroad, true);			
+			break;
+			
+		case DO_BUY:
+			Tools::debug("Logic: on_event: DO_BUY:");
+			// TODO definir 
+			vendedor = (char *)ev.tag;
+			mensaje = build_buy_msg(vendedor);
+			//add_nodo(mensaje, nombre_nodo);
+			
+			evBroad.id = BROADCAST;
+			evBroad.tag = Tools::duplicate(mensaje->to_string());
+			Router::instance()->post_event(evBroad, true);			
 			break;			
 			
 		case CLIENT_MSG:
 			Tools::debug("Logic: on_event: CLIENT_MSG:");
 			on_client_msg(ev.tag);
 			break;			
-			
-		case BUILD_MSG:
-			Tools::debug("Logic: on_event: BUILD_MSG:");
-			Event evSend;
-	    	evSend.id = SEND_BROADCAST_MSG;
-			evSend.tag = Tools::duplicate(build_msg());
-	    	Router::instance()->post_event(evSend, true); 			
-			break;						
-			
+						
 		default:
 			Tools::debug("Logic: on event: *UNKNOWN*.");
 			break;
@@ -58,87 +86,115 @@ void Logic::on_event(const Event& ev)
 }
 
 
-std::string Logic::build_msg()
+
+
+Mensaje *Logic::build_look_up_msg(std::string product_name, int cantidad)
 {
-	//<event ticket="Pepito0" />
-	//Tools::debug("Listener: armando mensaje de bienvenida al cliente");
-	using namespace xmlpp;
-	using namespace std;
-	Document doc;
-	Element* node = NULL;
-	Element* root = doc.create_root_node("event");
-
-	// TODO sacar este hardcode
-//	std::string id = Logic::instance()->add_client();
-	
-	std::string sockDesc("Pepito");
-	root->set_attribute("ticket", sockDesc);
-
-	return doc.write_to_string();
+	Mensaje *mensaje = new Mensaje();
+	mensaje->set_code(CODIGO_LOOKUP);
+	mensaje->set_product_name(product_name);
+	mensaje->set_cantidad(cantidad);
+	mensaje->set_hopcount(hopcount);
+	return mensaje;
 }
 
+Mensaje *Logic::build_reply_msg()
+{
+	Mensaje *mensaje = new Mensaje();
+	mensaje->set_code(CODIGO_REPLY);
+	mensaje->set_vendedor(nombre_nodo);
+	mensaje->set_cantidad(-1);
+	mensaje->set_hopcount(-1);
+	return mensaje;
+}
+
+Mensaje *Logic::build_buy_msg(std::string vendedor)
+{
+	Mensaje *mensaje = new Mensaje();
+	mensaje->set_code(CODIGO_BUY);
+	mensaje->set_vendedor(vendedor);
+	mensaje->set_cantidad(-1);
+	mensaje->set_hopcount(-1);
+	return mensaje;
+}
+
+
+/**
+Locked Add Nodo.
+*/
+void Logic::add_nodo(Mensaje *mensaje, const char*buffer)
+{
+	lock();
+	Nodo* nodo = new Nodo;
+	nodo->name = Tools::duplicate(buffer);
+	//clients[nodo->name] = nodo;
+	mensaje->get_nodos().push_back(nodo);
+	unlock();
+}
 
 
 /**
 Client msg handler:
 */
-void Logic::on_client_msg(const void* msg)
-{
-	using namespace xmlpp;
-	using namespace std;
-	assert(msg);
-
-	DomParser parser;
-	parser.parse_memory((const char*)msg);
-	Document* doc = parser.get_document();
-	Element* root = doc->get_root_node();
-	
-	//string client = get_attr_value(root, "client");
-	string client = Tools::get_attr_value(root, "ticket");
-	//int id = atoi(get_attr_value(root, "id").c_str());
-	//string timestamp = get_attr_value(root, "timestamp");
-	//Entity* e = (Entity*)clients[client];
-	//if (e == NULL)
-	//	return; //client was removed.
-
-	if (primeraVez && client == "Pepito")
-	{
-		Tools::debug("Logic: Es la primera vez que se ejecuta");
-		primeraVez = false;
-		Event ev;
-    	ev.id = BUILD_MSG;
-    	//ev.tag = duplicate(msg);
-    	this->post_event(ev, true);
-	}
-	else
-	{
-		Tools::debug("Logic: Es la segunda vez que se ejecuta y ultima");
-	}
-		
-		
+void Logic::on_client_msg(const void* xml_tag)
+{	
 	// Verificar si tengo lo que pide, si es Look up
 	// Verificar si el paquete es mio, si es un Replay
 	// Analizar todos lo casos..
 	// Si no me corresponde
 	// Decrementar el hopcount
 	// y enviar a mis vecinos
+		
+	assert(xml_tag);
+	std::string xml = (char *)xml_tag;
+	Mensaje *mensaje = new Mensaje();
+	mensaje->load(xml);
 	
 	
+	char logBuffer[BUFFER_SIZE];
+	memset(logBuffer, 0 , sizeof(logBuffer));
+	sprintf(logBuffer, "Logic: Mensaje recibido [%s]", xml.c_str());
+	Tools::debug(logBuffer);
 	
-	int id = -1;
-	//Process event:
-	switch (id)
-	{		
-		case LOOKUP:
-			Tools::debug("Logic: on_event: LOOKUP:");
-			break;
-			
-		case REPLAY:
-			Tools::debug("Logic: on_event: REPLAY:");
-			break;
-						
-		case BUY:
-			Tools::debug("Logic: on_event: BUY:");		
-			break;
+	//memset(logBuffer, 0 , sizeof(logBuffer));
+	//sprintf(logBuffer, "Logic: mensaje->code [%s]", mensaje->get_code().c_str());
+	//Tools::debug(logBuffer);
+	
+	if (mensaje->get_code().compare(CODIGO_LOOKUP) == 0) 
+	{
+		memset(logBuffer, 0 , sizeof(logBuffer));
+		sprintf(logBuffer, "Logic: Es un lookup. Tengo [%d] unidades de [%s]?", 
+				mensaje->get_cantidad(), mensaje->get_product_name().c_str());
+		Tools::debug(logBuffer);
+		
+		Event ev;
+		ev.id = DO_REPLY;
+		this->post_event(ev, true);
+	}
+	else if (mensaje->get_code().compare(CODIGO_REPLY) == 0)
+	{
+		Tools::debug("Logic: codigo REPLY:");
+		
+		Event ev;
+		ev.id = DO_BUY;
+		ev.tag = Tools::duplicate(mensaje->get_vendedor());
+		this->post_event(ev, true);
+	}
+	else if (mensaje->get_code().compare(CODIGO_BUY) == 0)
+	{
+		Tools::debug("Logic: codigo BUY:");
+		// Preparar todo para recibir un connect del comprador.
 	}
 }
+
+
+/**
+Getters:
+*/
+//Mensaje& Logic::get_mensaje()
+//{
+//	return mensaje;
+//}
+
+
+
--- trunk/src/logic.h
+++ trunk/src/logic.h
@@ -2,6 +2,8 @@
 #define _LOGIC_H_
 
 #include "common/runner.h"
+#include "common/mensaje.h"
+#include "common/nodo.h"
 
 class Logic : public Runner
 {
@@ -16,18 +18,23 @@ class Logic : public Runner
 		*/
 		virtual void on_event(const Event& ev);
 
-		std::string build_msg();
+		Mensaje *build_look_up_msg(std::string product_name, int cantidad);
+		Mensaje *build_reply_msg();
+		Mensaje *build_buy_msg(std::string vendedor);
 		
 		void on_client_msg(const void* msg);
+		
+		//Mensaje& get_mensaje();
+		
 	protected:	
-
+		void add_nodo(Mensaje *mensaje, const char*buffer);
 		
 	
 	protected:
 		Logic();
 		~Logic();
 		static Logic* single_instance;				
-		
+		int hopcount;		
 };
 
 #endif //_LOGIC_H_
