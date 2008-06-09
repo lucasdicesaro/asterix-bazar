11d3e9b1214ec5839a4e41ccf7a202fb9b53fe99
gbertossi <gbertossi@d3c0cedc-9d4d-0410-b2cd-271dfcd79d49>
2008-06-09 02:26:36 +0000

    se completaron los metodos de stock

--- src/stock.h
+++ src/stock.h
@@ -3,25 +3,28 @@
 
 #include "common/tools.h"
 #include "common/types.h"
-
+#include <map>
+#include <string>
 
 class Stock
 {
 	public:
-		
+	
 		/**
 		Singleton
 		*/
 		static Stock* instance();
 		
 		void load_stock();
-		void decrement_stock(char* product_name, int cantidad);
-		void increment_stock(char* product_name, int cantidad);
+		int get_stock(std::string product_name);
+		void decrement_stock(std::string product_name, int cantidad);
+		void increment_stock(std::string product_name, int cantidad);
 			
 	protected:
 		Stock();
 		~Stock();
 		static Stock* single_instance;
+		std::map<std::string, int> mapa_stock;
 };
 
 #endif //_STOCK_H_
