0b6d91d8fedf5aa358ed4433787b136323589125
gbertossi <gbertossi@d3c0cedc-9d4d-0410-b2cd-271dfcd79d49>
2008-06-09 02:26:36 +0000

    se completaron los metodos de stock

--- trunk/src/stock.cpp
+++ trunk/src/stock.cpp
@@ -7,6 +7,9 @@ Stock* Stock::single_instance = NULL;
 */
 Stock::Stock()
 {
+	mapa_stock["sal"] = 0;
+	mapa_stock["pollo"] = 0;
+	mapa_stock["verdura"] = 0;
 }
 
 Stock::~Stock()
@@ -24,10 +27,23 @@ void Stock::load_stock()
 {    
 }
 
-void Stock::decrement_stock(char* product_name, int cantidad)
+void Stock::decrement_stock(const std::string product_name, int cantidad)
 {
+	if (mapa_stock.count(product_name) > 0 && mapa_stock[product_name] > cantidad) 
+		mapa_stock[product_name] = mapa_stock[product_name]-cantidad;
+	//TODO: Como levantar el error si decrementa a menos de 0?
 }
 
-void Stock::increment_stock(char* product_name, int cantidad)
+void Stock::increment_stock(const std::string product_name, int cantidad)
 {
+	if (mapa_stock.count(product_name) > 0) 
+		mapa_stock[product_name] = mapa_stock[product_name] + cantidad;
+}
+
+int Stock::get_stock(const std::string product_name)
+{
+	if (mapa_stock.count(product_name) > 0) 
+		return mapa_stock[product_name];
+	else
+		return -1;
 }
