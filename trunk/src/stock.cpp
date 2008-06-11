e7c6bbccd0374688baebc431510e3e5ce255ad28
gbertossi <gbertossi@d3c0cedc-9d4d-0410-b2cd-271dfcd79d49>
2008-06-11 13:18:22 +0000

    agregados seter y getter para  compro y vendo - cambio de tipos

--- trunk/src/stock.cpp
+++ trunk/src/stock.cpp
@@ -8,8 +8,8 @@ Stock* Stock::single_instance = NULL;
 Stock::Stock()
 {
 	mapa_stock["sal"] = 0;
-	mapa_stock["pollo"] = 0;
-	mapa_stock["verdura"] = 0;
+	mapa_stock["pescado"] = 0;
+	mapa_stock["verdura"] = 10;
 }
 
 Stock::~Stock()
@@ -27,23 +27,43 @@ void Stock::load_stock()
 {    
 }
 
-void Stock::decrement_stock(const std::string product_name, int cantidad)
+void Stock::decrement_stock(char* product_name, int cantidad)
 {
 	if (mapa_stock.count(product_name) > 0 && mapa_stock[product_name] > cantidad) 
 		mapa_stock[product_name] = mapa_stock[product_name]-cantidad;
 	//TODO: Como levantar el error si decrementa a menos de 0?
 }
 
-void Stock::increment_stock(const std::string product_name, int cantidad)
+void Stock::increment_stock(char* product_name, int cantidad)
 {
 	if (mapa_stock.count(product_name) > 0) 
 		mapa_stock[product_name] = mapa_stock[product_name] + cantidad;
 }
 
-int Stock::get_stock(const std::string product_name)
+int Stock::get_stock(std::string product_name)
 {
 	if (mapa_stock.count(product_name) > 0) 
 		return mapa_stock[product_name];
 	else
 		return -1;
+}
+
+char* Stock::get_compro()
+{
+        return compro;
+}
+
+void Stock::set_compro(char* product_name)
+{
+        compro = product_name;
+}
+
+char* Stock::get_vendo()
+{
+        return vendo;
+}
+
+void Stock::set_vendo(char* product_name)
+{
+        vendo = product_name;
 }
