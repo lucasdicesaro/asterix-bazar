449876b41d2e79bc548ea99b0a035477e364cd1b
gbertossi <gbertossi@d3c0cedc-9d4d-0410-b2cd-271dfcd79d49>
2008-06-11 13:18:22 +0000

    agregados seter y getter para  compro y vendo - cambio de tipos

--- src/stock.h
+++ src/stock.h
@@ -4,7 +4,6 @@
 #include "common/tools.h"
 #include "common/types.h"
 #include <map>
-#include <string>
 
 class Stock
 {
@@ -17,14 +16,20 @@ class Stock
 		
 		void load_stock();
 		int get_stock(std::string product_name);
-		void decrement_stock(std::string product_name, int cantidad);
-		void increment_stock(std::string product_name, int cantidad);
+		void decrement_stock(char* product_name, int cantidad);
+		void increment_stock(char* product_name, int cantidad);
+		char* get_compro();
+		void set_compro(char* product_name);
+		char* get_vendo();
+		void set_vendo(char* product_name);
 			
 	protected:
 		Stock();
 		~Stock();
 		static Stock* single_instance;
 		std::map<std::string, int> mapa_stock;
+        char* compro;
+        char* vendo;
 };
 
 #endif //_STOCK_H_
